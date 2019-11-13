205becaffa6a6efbb60a570eb8936471a9bae4e9
MAHESH897 <57609622+MAHESH897@users.noreply.github.com>
2019-11-13 08:49:51 +0530

    Create amogh.java

---
+++ amogh.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, amogh");
+ System.out.println("Hello besant");
+ System.out.println("welcome besant");
+  }
+}
